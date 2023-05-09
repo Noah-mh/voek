0947b51640dd559883a0af698a70cb2b93974a5e
Allison <109326942+ally-lh@users.noreply.github.com>
2023-05-10 01:22:38 +0800

    feature_footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/header/header.tsx";
 import LoginBanner from "./components/loginBanner/loginBanner.tsx";
+import Footer from "./components/footer/footer.tsx";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Header />
       <LoginBanner />
+      <Footer mode="light" />
     </div>
   );
 }
