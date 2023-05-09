7ad025d84933125f20c453ba545d7cb2fe36cf7b
Allison <109326942+ally-lh@users.noreply.github.com>
2023-05-10 01:28:51 +0800

    Revert "Front - Footer"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,14 @@
-import { useState } from "react";
+import { useState } from 'react'
 import "./App.css";
-import Header from "./components/header/header.tsx";
-import LoginBanner from "./components/loginBanner/loginBanner.tsx";
-import Footer from "./components/footer/footer.tsx";
-import { Route, Routes } from "react-router-dom";
+
 
 function App() {
+
   return (
-    <div>
-      <Header />
-      <LoginBanner />
-      <Footer mode="light" />
-    </div>
-  );
+    <>
+      <div className='text-5xl font-bold'>Hello World</div>
+    </>
+  )
 }
 
-export default App;
+export default App
--- server/config/sendInBlue.ts
+++
@@ -1,8 +0,0 @@
-var Sib = require('sib-api-v3-sdk');
-import config from './config'
-
-const client = Sib.ApiClient.instance;
-const apiKey = client.authentications['api-key'];
-apiKey.apiKey = config.sendInBlueAPIKey;
-
-export default Sib;
