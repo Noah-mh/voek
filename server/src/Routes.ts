caf0aaad9dea84eea3561fb71adf02f606ff6911
Kang Rui <tankangrui654321@gmail.com>
2023-05-10 00:49:19 +0800

    process OTP

--- server/src/Routes.ts
+++ server/src/Routes.ts
@@ -1,8 +1,9 @@
 import { Express, Request, Response } from "express";
-import { processLogin, processSendEmailOTP, processSendSMSOTP } from "./controller/customer.controller";
+import { processLogin, processSendEmailOTP, processSendSMSOTP, processVerifyOTP } from "./controller/customer.controller";
 
 export default function (app: Express) {
     app.post('/login', processLogin);
     app.post('/auth/SMS/OTP', processSendSMSOTP);
     app.post('/auth/email/OTP', processSendEmailOTP);
+    app.post('/auth/OTP', processVerifyOTP);
 }
