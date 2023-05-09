6adcac713d38c424352215efbd4481875f3d2348
ashleyteo404 <ashleyteo404@gmail.com>
2023-05-09 13:56:45 +0800

    Get All Products From 1 Seller

--- server/src/Routes.ts
+++ server/src/Routes.ts
@@ -1,7 +1,10 @@
-import { Express, Request, Response } from "express"
+import { Express, Request, Response } from 'express'
+import { processGetAllProductsOfSeller } from './controller/seller.controller'
 
 export default function (app: Express) {
     app.get('/', (req: Request, res: Response) => {
         res.send('Done')
     })
+
+    app.get('/products/:sellerId', processGetAllProductsOfSeller);
 }
