c2c908beae5854037ce04e4ae104db011c0ee343
Kang Rui <tankangrui654321@gmail.com>
2023-05-10 11:19:57 +0800

    some changes

--- server/src/Routes.ts
+++ server/src/Routes.ts
@@ -1,8 +1,32 @@
 import { Express, Request, Response } from "express";
-import { processLogin, processSendEmailOTP, processSendSMSOTP } from "./controller/customer.controller";
+import { processLogin, processSendEmailOTP, processSendSMSOTP, processVerifyOTP, processSignUp, processSendEmailLink, processSignUpLink } from "./controller/customer.controller";
+import verifyJWT from "./middlewares/verifyJwt";
+import verifyRoles from "./middlewares/verifyRoles";
+import {
+  processPublicProductDetails,
+  processCartDetails,
+  getRecommendedProductsBasedOnCat,
+  getWishlistItems,
+  getLastViewed
+} from "./controller/product.controller";
+
 
 export default function (app: Express) {
-    app.post('/login', processLogin);
-    app.post('/auth/SMS/OTP', processSendSMSOTP);
-    app.post('/auth/email/OTP', processSendEmailOTP);
+
+  // KANG RUI ENDPOINTS
+  app.post('/login', processLogin);
+  app.post('/auth/SMS/OTP', processSendSMSOTP);
+  app.post('/auth/email/OTP', processSendEmailOTP);
+  app.post('/auth/verify/OTP', processVerifyOTP);
+  app.post('/signup/link', processSendEmailLink);
+  app.post('/signup/verify/link', processSignUpLink);
+  app.post('/signup', processSignUp);
+  //
+  app.get("/getRecommendedProductsBasedOnCat", getRecommendedProductsBasedOnCat);
+  app.get("/getWishlistItems", getWishlistItems);
+  app.get("/getLastViewed", getLastViewed);
+
+  app.get("/productDetails", processPublicProductDetails);
+  app.get("/cartDetails", processCartDetails);
 }
+
