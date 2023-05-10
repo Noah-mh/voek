5248079ae75809263da93268b36e374b7cb079d1
Zolastic <honhattienlul@gmail.com>
2023-05-11 01:52:02 +0800

    added homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/header/header.tsx";
-import LoginBanner from "./components/loginBanner/loginBanner.tsx"; 
+import LoginBanner from "./components/loginBanner/loginBanner.tsx";
 import Footer from "./components/footer/footer.tsx";
+import Homepage from "./components/homepage/Homepage.tsx";
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -10,7 +12,8 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<LoginBanner />} />
+        <Route path="/LoginBanner" element={<LoginBanner />} />
+        <Route path="/" element={<Homepage />} />
       </Routes>
       <Footer />
     </Router>
