a438bb9ac54ef42a826cec8e5b6f0b1dccdbceb0
Zolastic <honhattienlul@gmail.com>
2023-05-10 13:26:51 +0800

    commented a part on in customer.controller.ts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/header/header.tsx";
-import LoginBanner from "./components/loginBanner/loginBanner.tsx";
+import LoginBanner from "./components/loginBanner/loginBanner.tsx"; 
 import Footer from "./components/footer/footer.tsx";
 
 function App() {
