a36a6327f547dd68f491e62474469a71c0be5c86
Noah <105475352+Noah-mh@users.noreply.github.com>
2023-05-12 00:25:59 +0800

    Update Layout.tsx

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import Header from '../header/Header.js';
+import Header from '../header/header.js';
 import Footer from '../footer/footer.js';
 
 const Layout = () => {
