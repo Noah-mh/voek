c6ba605b943030679b6c672b2560f10860a3879e
Noah <105475352+Noah-mh@users.noreply.github.com>
2023-05-17 13:18:29 +0800

    adding review endpoint

--- server/src/Routes.ts
+++ server/src/Routes.ts
@@ -1,3 +1,4 @@
+import { addingReview } from "./controller/review.controller";
 import { Express, Router } from "express";
 import verifyJWT from "./middlewares/verifyJWT";
 import verifyRoles from "./middlewares/verifyRoles";
@@ -7,6 +8,7 @@ import * as authController from "./controller/auth.controller";
 import * as sellerController from "./controller/seller.controller";
 import * as cartController from "./controller/cart.controller";
 import * as orderController from "./controller/order.controller";
+import * as reviewController from "./controller/review.controller";
 
 export default function (app: Express, router: Router) {
   // KANG RUI ENDPOINTS - user management system
@@ -78,10 +80,22 @@ export default function (app: Express, router: Router) {
     "/seller/verify/reset/password",
     sellerController.processForgetPasswordLink
   );
-  router.post("/seller/reset/password", sellerController.processResetPassword);
-  router.get('/customer/orders/:customer_id', orderController.processHandleGetCustomerOrders)
-  router.get('/customer/delivered/orders/:customer_id', orderController.processhandleGetCustomerDeliveredOrders)
-  router.get('/customer/received/orders/:customer_id', orderController.processGetCustomerReceivedOrders)
+  router.post(
+    "/seller/reset/password",
+    sellerController.processResetPassword
+  );
+  router.get(
+    "/customer/orders/:customer_id",
+    orderController.processHandleGetCustomerOrders
+  );
+  router.get(
+    "/customer/delivered/orders/:customer_id",
+    orderController.processhandleGetCustomerDeliveredOrders
+  );
+  router.get(
+    "/customer/received/orders/:customer_id",
+    orderController.processGetCustomerReceivedOrders
+  );
 
   // NOAH ENDPOINTS - reviews
   router.get(
@@ -92,6 +106,8 @@ export default function (app: Express, router: Router) {
     "/productReviews/:product_id",
     productController.getProductReviews
   );
+  router.get("/addReview", reviewController.addingReview);
+  router.get("/addReviewImages", reviewController.addingReviewImages);
 
   // ASHLEY ENDPOINTS - seller platform
   router.get(
