846b3504115eda3d0d33d65eac82bae1e07eb6a8
Noah <105475352+Noah-mh@users.noreply.github.com>
2023-05-24 16:15:03 +0800

    env config

--- client/src/api/axios.tsx
+++ client/src/api/axios.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-const BASE_URL = "https://ades-voek-server.onrender.com";
-// const BASE_URL = "http://localhost:3500";
+
+const BASE_URL = import.meta.env.VITE_APP_BACKEND_BASE_URL || "http://localhost:3500";
 
 export default axios.create({
   baseURL: BASE_URL,
--- server/config/allowedOrigin.ts
+++ server/config/allowedOrigin.ts
@@ -1,7 +1,14 @@
+
+import * as dotenv from 'dotenv';
+dotenv.config({
+  path: __dirname + '../../env'
+});
+
+
 const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
-  "https://main--voek.netlify.app",
   "https://ades-voek-client.onrender.com",
+  process.env.FRONTEND_BASE_URL,
 ];
 export default allowedOrigins;
