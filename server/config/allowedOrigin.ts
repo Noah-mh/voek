ab1c96deb9c46768c27c5593764e1310d098a656
Noah <105475352+Noah-mh@users.noreply.github.com>
2023-05-24 16:23:18 +0800

    testing

--- server/config/allowedOrigin.ts
+++ server/config/allowedOrigin.ts
@@ -8,6 +8,7 @@ dotenv.config({
 const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
+  "https://main--voek.netlify.app",
   "https://ades-voek-client.onrender.com",
   process.env.FRONTEND_BASE_URL,
 ];
