60c91c08e3e4c80fb6e8683f8381bfc00bd11a60
Noah <105475352+Noah-mh@users.noreply.github.com>
2023-05-26 02:40:49 +0800

    endpoints for seller page

---
+++ server/src/model/customer_seller.model.ts
@@ -0,0 +1,113 @@
+import pool from "../../config/database";
+
+export const handleSellerDetails = async (product_id: number) => {
+  const promisePool = pool.promise();
+  const connection = await promisePool.getConnection();
+  const sql = `SELECT p.product_id, s.seller_id, s.shop_name, s.image_url, COUNT(lp.product_id) AS total_product
+  FROM products p
+  LEFT JOIN listed_products lp ON p.product_id = lp.product_id
+  INNER JOIN seller s ON lp.seller_id = s.seller_id
+  WHERE p.product_id = ?
+  GROUP BY p.product_id, s.seller_id, s.shop_name, s.image_url;`;
+  try {
+    const [result] = await connection.query(sql, [product_id]);
+    console.log(result);
+    return result as seller[];
+  } catch (err: any) {
+    throw new Error(err);
+  } finally {
+    await connection.release();
+  }
+};
+
+export const handleSellerCategories = async (seller_id: number) => {
+  const promisePool = pool.promise();
+  const connection = await promisePool.getConnection();
+  const sql = `SELECT DISTINCT c.category_id, c.name as category_name
+  FROM listed_products lp
+  INNER JOIN products p ON lp.product_id = p.product_id
+  INNER JOIN category c ON p.category_id = c.category_id
+  WHERE lp.seller_id = ?;`;
+  try {
+    const [result] = await connection.query(sql, [seller_id]);
+    console.log(result);
+    return result as Category[];
+  } catch (err: any) {
+    throw new Error(err);
+  } finally {
+    await connection.release();
+  }
+};
+
+export const handleSellerProductsDetails = async (
+  seller_id: number
+) => {
+  const promisePool = pool.promise();
+  const connection = await promisePool.getConnection();
+  const sql = `SELECT p.product_id, p.name, pv.price,pv.sku,
+  ROUND(AVG(r.rating), 2) as rating,c.category_id, c.name as category_name
+ FROM listed_products lp
+ INNER JOIN products p ON lp.product_id = p.product_id
+ INNER JOIN product_variations pv ON p.product_id = pv.product_id
+ INNER JOIN category c ON p.category_id = c.category_id
+ LEFT JOIN review r ON pv.sku = r.sku
+ WHERE lp.seller_id = ? 
+ GROUP BY p.product_id, p.name, pv.price, pv.sku, c.category_id, c.name;`;
+  try {
+    const [result] = await connection.query(sql, [seller_id]);
+    console.log(result);
+    return result as Product[];
+  } catch (err: any) {
+    throw new Error(err);
+  } finally {
+    await connection.release();
+  }
+};
+
+export const handleSellerProductsByCategory = async (
+  seller: number,
+  category: number
+) => {
+  const promisePool = pool.promise();
+  const connection = await promisePool.getConnection();
+  const sql = `SELECT p.product_id, p.name, pv.price,pv.sku,
+  ROUND(AVG(r.rating), 2) as rating,c.category_id, c.name as category_name
+  FROM listed_products lp
+  INNER JOIN products p ON lp.product_id = p.product_id
+  INNER JOIN product_variations pv ON p.product_id = pv.product_id
+  INNER JOIN category c ON p.category_id = c.category_id
+  LEFT JOIN review r ON pv.sku = r.sku
+  WHERE lp.seller_id = ? AND c.category_id = ?
+  GROUP BY p.product_id, p.name, pv.price, pv.sku, c.category_id, c.name;`;
+  try {
+    const [result] = await connection.query(sql, [seller, category]);
+    console.log(result);
+    return result as Product[];
+  } catch (err: any) {
+    throw new Error(err);
+  } finally {
+    await connection.release();
+  }
+};
+
+interface Product {
+  product_id: number;
+  name: string;
+  price: number;
+  sku: string;
+  rating: number;
+  category_id: number;
+  category_name: string;
+}
+
+interface seller {
+  seller_id: number;
+  shop_name: string;
+  image_url: string;
+  total_product: number;
+}
+
+interface Category {
+  category_id: number;
+  name: string;
+}
