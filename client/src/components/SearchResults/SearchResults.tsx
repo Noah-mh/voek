2ce7c793f92ea9b94613e32c5d5131f998a169b4
Zolastic <honhattienlul@gmail.com>
2023-07-07 23:08:04 +0800

    added a catch to a try catch in product.model.ts

--- client/src/components/SearchResults/SearchResults.tsx
+++ client/src/components/SearchResults/SearchResults.tsx
@@ -12,9 +12,7 @@ const SearchResults = () => {
     setInput(userInput);
   }, [userInput]);
 
-  return (
-    <div className="">{input !== "" && <Products userInput={input} />}</div>
-  );
+  return <div>{input !== "" && <Products userInput={input} />}</div>;
 };
 
 export default SearchResults;
