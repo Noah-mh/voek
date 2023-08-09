266f3438b33a6760e2bec353cc4b3a01d6eb0b9b
Allison <109326942+ally-lh@users.noreply.github.com>
2023-08-09 19:50:09 +0800

    Update Usercart: Disable coins and give feedback  if total is too low

--- client/src/components/UserCart/UserCart.tsx
+++ client/src/components/UserCart/UserCart.tsx
@@ -294,6 +294,7 @@ export default function cartPage(): JSX.Element {
         setChangedQuantState(false);
         getUserCart();
         setClaimedVouchers({});
+        setIsChecked(false);
       } catch (err: any) {
         console.log(err);
       }
@@ -306,6 +307,17 @@ export default function cartPage(): JSX.Element {
     const discAmt = Math.floor(userCoins / 10);
     if (isChecked) {
       if (totalAmt.total < discAmt) {
+        toast.warn("Your total is too low for coins to be applied.", {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+        setIsChecked(false);
         return;
       } else {
         setTotalAmt({
@@ -786,6 +798,7 @@ export default function cartPage(): JSX.Element {
                 onChange={() => {
                   isChecked ? setIsChecked(false) : setIsChecked(true);
                 }}
+                checked={isChecked}
               />
               <div className="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
             </label>
